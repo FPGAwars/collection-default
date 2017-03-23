066a36cd9db99ff7ecb93e1fc99c7622e8d1b1b7
obijuan <juan@iearobotics.com>
2017-03-23 19:49:25 +0100

    icm update executed

--- README.md
+++ README.md
@@ -1,76 +1,93 @@
-## Default Collection
+# Default
 
-Default collection in *Icestudio 0.2*.
+[![Icestudio](https://img.shields.io/badge/collection-icestudio-blue.svg)](https://github.com/FPGAwars/icestudio)
+[![Version](https://img.shields.io/badge/version-v0.2.0-orange.svg)]()
 
-### Install
+Default collection in Icestudio 0.2.
 
-* Download the [collection](https://github.com/FPGAwars/collection-default/archive/v0.2.0.zip).
-* Go to *Tools > Collections > Add* to install the collection.
-* Go to *Select > Collections* to view the installed collection.
+## Install
 
-### Blocks
-* Bit
+* Download the [collection](https://github.com/FPGAwars/collection-default/archive/v0.2.0.zip)
+* Go to *Tools > Collections > Add* to install the collection
+* Go to *Select > Collections* to view the installed collection
+
+## Blocks
+* bit
   * 0
   * 1
-* Config
-  * Pull up
-  * Pull up inv
-  * Tri-state
-* Logic
-  * Combinational
-    * Demux 1:2
-    * Demux 1:4
-    * Demux 1:8
-    * Hex 7 Segment CA
-    * Hex 7 Segment CC
-    * Mux 2:1
-    * Mux 4:1
-  * Gate
-    * And
-    * Nand
-    * Nor
-    * Not
-    * Or
-    * Xnor
-    * Xnor
-  * Sequential
-    * Debouncer
-    * D flip-flop async
-    * D flip-flop
-    * T flip-flop async
-    * T flip-flop
+* config
+  * pull_up
+* logic
+  * combinational
+    * mux_2_1
+  * gate
+    * and
+    * not
+    * or
+    * xor
+  * sequential
+    * debouncer
+    * dff
+    * prescaler_22
+    * prescaler_N
+    * tff
 
-### Examples
-* Basic
-  * Led on
-  * Switch led
-  * Switch and gate
-* Counter
-  * contadorAsc
-  * contadorDes
+## Examples
+* 1_basic
+  * 1_one_led
+  * 2_two_leds
+  * 3_three_leds
+  * 4_blink
+  * 5_two_led_alternate_blink
+  * 6_pushbutton_led
+* 2_flip_flops
+  * 1_Dff_manual_load
+  * 2_chain_of_three_DFF
+  * 3_ring_of_four_DFFs
+  * 4_toggle_button
+  * 5_binary_counter_with_three_TFFs
+* 3_gates
+  * 01_and
+  * 02_or
+  * 03_and_truth_table
+  * 04_or_truth_table
+  * 05_sr_latch
+  * 06_D_latch
+  * 07_D_flip_flop
+  * 08_mux_2_1
+  * 09_two_to_four_decoder
+  * 10_one_bit_adder
+* 4_muxes
+  * 1_Blinking_fixed_led
+  * 2_led_three_states
+  * 3_mux_4_1
+  * 4_shift_register
+* 5_verilog
+  * 01_four_bits_fixed_binary_value
+  * 02_four_bits_fixed_hexa_value
+  * 03_eight_bits_fixed_value_buses
+  * 04_eight_bits_counter
+  * 05_split_buses
+  * 06_rom16x8
+  * 07_rom16x8_file
+  * 08_rom16x8_file_generic
 
-### Languages
+## Languages
+| Language | Translated strings |
+|:--------:|:------------------:|
+| en | ![Progress](http://progressed.io/bar/100) |
+| es_ES | ![Progress](http://progressed.io/bar/100) |
+| eu_ES | ![Progress](http://progressed.io/bar/24) |
+| fr_FR | ![Progress](http://progressed.io/bar/24) |
+| gl_ES | ![Progress](http://progressed.io/bar/20) |
 
-|  Language  | Translated strings                         |
-|:----------:|:------------------------------------------:|
-| English    | ![Progress](http://progressed.io/bar/100)  |
-| Spanish    | ![Progress](http://progressed.io/bar/100)  |
-| French     | ![Progress](http://progressed.io/bar/100)  |
-| Basque     | ![Progress](http://progressed.io/bar/97)   |
-| Galician   | ![Progress](http://progressed.io/bar/82)   |
+## Authors
+* [Jesús Arroyo](https://github.com/Jesus89)
+* [Juan González](https://github.com/Obijuan)
+* [Carlos Díaz](https://github.com/C47D)
+* [Salvador E. Tropea](https://github.com/set-soft)
 
-### Authors
- * [Jesús Arroyo Torrens](https://github.com/Jesus89)
- * [Juan González (Obijuan)](https://github.com/Obijuan)
- * [Carlos Díaz](https://github.com/C47D)
- * [Salvador E. Tropea](https://github.com/set-soft)
 
-### Contributors
- * [Xoan Sampaiño](https://github.com/xoan)
- * [Lorea-Aldabaldetreku](https://github.com/Lorea-Aldabaldetreku)
- * [Martoni](https://github.com/Martoni)
- * [Pascal Cotret](https://github.com/pcotret)
+## License
 
-### License
-
-Licensed under [GPL 2.0](https://opensource.org/licenses/GPL-2.0)
+Licensed under [GPL-2.0](https://opensource.org/licenses/GPL-2.0).
--- locale/translation.js
+++ locale/translation.js
@@ -1,141 +1,121 @@
-// Translation document for the Default collection
-// ===============================================
-// This file contains the labels and comments annotated for translation
+// Translation document for the collection
+// =======================================
+// This file contains the texts
+// annotated for translation
 //
-// IMPORTANT: Configure Poedit:
-// 1. Go to Edit > Preferences > Extractors
-// 2. Select "JavaScript" and press "Edit"
-// 3. Change the "Command to extract translations:" field:
-//   - Replace the text "TRANSLATORS:" by a slash
-// 4. Press "OK" and "Close"
+// Instructions:
+// 1. Open the PO file with Poedit
+// 2. Go to "Catalog > Properties > Sources paths"
+// 3. Add the locale/translation.js file
+// 4. Press "Update" to update from sources
 
-
-/// Bit
 gettext('bit');
-gettext('Assign 0 to the output wire');
-gettext('Assign 1 to the output wire');
-/// Config
 gettext('config');
-/// Pull up
-gettext('pull_up');
-gettext('FPGA internal pull up configuration on the connected input port')
-/// Logic
 gettext('logic');
-/// Combinational
+gettext('0');
+gettext('Assign 0 to the output wire');
+gettext('1');
+gettext('Assign 1 to the output wire');
+gettext('pull_up');
+gettext('FPGA internal pull up configuration on the connected input port');
 gettext('combinational');
-/// Mux 2:1
+gettext('gate');
+gettext('sequential');
 gettext('mux_2_1');
 gettext('Multiplexer 2:1');
-/// Gate
-gettext('gate');
-/// And
 gettext('and');
 gettext('AND logic gate');
-/// Not
 gettext('not');
 gettext('NOT logic gate');
-/// Or
 gettext('or');
 gettext('OR logic gate');
-/// Xor
-gettext('XOR logic gate');
 gettext('xor');
-/// Sequential
-gettext('sequential');
-/// D flip-flop
-gettext('dff');
-/// T flip-flop
-gettext('tff');
-/// Debouncer
+gettext('XOR logic gate');
 gettext('debouncer');
-/// Prescaler 22
+gettext('Remove the rebound on a mechanical switch');
+gettext('dff');
+gettext('Delay flip-flop with synchronous reset');
 gettext('prescaler_22');
 gettext('22-bits prescaler');
-/// Prescaler N
-gettext('prescaler_N');
 gettext('Parametric N-bits prescaler');
-/// 1. Basic
+gettext('prescaler_N');
+gettext('tff');
+gettext('Toggle flip-flop with synchronous reset');
 gettext('1_basic');
-/// 1. One led
-gettext('1_one_led');
-/// 2. Two leds
-gettext('2_two_leds');
-/// 3. Three leds
-gettext('3_three_leds');
-/// 4. Blink
-gettext('4_blink');
-/// 5. Two leds alternate blink
-gettext('5_two_led_alternate_blink');
-/// 6. Pushbutton led
-gettext('6_pushbutton_led');
-/// 2. Flip-Flips
 gettext('2_flip_flops');
-/// 1. D-flip-flop manual load
-gettext('1_Dff_manual_load');
-/// 2. Chain of three DFF
-gettext('2_chain_of_three_DFF');
-/// 3. Ring of four DFF
-gettext('3_ring_of_four_DFFs');
-/// 4. Toggle button
-gettext('4_toggle_button');
-/// 5. Binary counter with three T Flip-flops
-gettext('5_binary_counter_with_three_TFFs');
-/// 3. Gates
 gettext('3_gates');
-/// 1. And gate
-gettext('01_and');
-/// 2. Or gate
-gettext('02_or');
-/// 3. And truth table
-gettext('03_and_truth_table');
-/// 4. Or truth table
-gettext('04_or_truth_table');
-/// 5. SR latch
-gettext('05_sr_latch');
-/// 6. D latch
-gettext('06_D_latch');
-/// 7. D flip-flop
-gettext('07_D_flip_flop');
-/// 8. Mux 2:1
-gettext('08_mux_2_1');
-/// 9. Two-to-four decoder
-gettext('09_two_to_four_decoder');
-/// 10. One bit adder
-gettext('10_one_bit_adder');
-/// 4. Muxes
 gettext('4_muxes');
-/// 1. Blinking/fixed led
-gettext('1_Blinking_fixed_led');
-/// 2. Led three states
-gettext('2_led_three_states');
-/// 3. Mux 4:1
-gettext('3_mux_4_1');
-/// 4. Shift Register
-gettext('4_shift_register');
-/// 5. Verilog
 gettext('5_verilog');
-/// 1. Four bits fixed binary value 
+gettext('1_one_led');
+gettext('Turn on a led');
+gettext('2_two_leds');
+gettext('Turn on two leds');
+gettext('3_three_leds');
+gettext('Turn on three leds');
+gettext('4_blink');
+gettext('Blinking led');
+gettext('5_two_led_alternate_blink');
+gettext('6_pushbutton_led');
+gettext('One pushbutton for turning the led on and off');
+gettext('1_Dff_manual_load');
+gettext('The led is turned on when the pushbutton is pressed');
+gettext('2_chain_of_three_DFF');
+gettext('A chain of three D flip-flops\nThe leds are turned on secuentially, with every click on the button');
+gettext('3_ring_of_four_DFFs');
+gettext('A ring of four D flip-flops\nThe sequence is shown in the leds');
+gettext('4_toggle_button');
+gettext('When the pushbutton is clicked the led changes');
+gettext('5_binary_counter_with_three_TFFs');
+gettext('A 3-bit binary counter with T flip-flops');
+gettext('01_and');
+gettext('Testing the AND gate');
+gettext('02_or');
+gettext('Testing the OR gate');
+gettext('03_and_truth_table');
+gettext('Truth table of the AND gate');
+gettext('04_or_truth_table');
+gettext('Truth table of the OR gate');
+gettext('05_sr_latch');
+gettext('An SR-latch implemented with logic gates');
+gettext('06_D_latch');
+gettext('An D latch implemented with logic gates');
+gettext('07_D_flip_flop');
+gettext('Master Latch');
+gettext('Slave latch');
+gettext('A D flip-flip with a falling-edge trigger implemented with logic gates');
+gettext('08_mux_2_1');
+gettext('Mux 2:1 implemented with logic gates\nThe led0 blinks or is turned on depending on the pushbutton 1');
+gettext('Mux 2:1 with logic gates');
+gettext('Signal for blinking the led');
+gettext('Fixed signal for turning on the led');
+gettext('09_two_to_four_decoder');
+gettext('2 to 4 bits decoder with logic gates');
+gettext('10_one_bit_adder');
+gettext('1-bit adder');
+gettext('Sum');
+gettext('Carry');
+gettext('1_Blinking_fixed_led');
+gettext('Blinking/fixed led\nThe toggle button selects if the led blinks or stays turned on');
+gettext('2_led_three_states');
+gettext('The led has three modes of operation: blinking, turned on an turned off\nSW2 pushbutton determines if the led blinks/stays fixed\nSW1 pushbutton determines the led state (on or off), in fixed mode');
+gettext('3_mux_4_1');
+gettext('A 4:1 mux from two 2:1 muxes\nThe led can be in four states: off, blinking, on, blinking fast');
+gettext('4_shift_register');
+gettext('4 bits shift register. It is loaded with the initial value 1000 and then\nshifted on every clock pulse');
+gettext('Binary value to load and shift');
+gettext('Initializer. It has the initial value 0, so that the\ni0 is selected in all the muxes. The register is loaded.\nIn the next cycle, it has the value 1, so the i1 input\nis selected in all the muxes. The register is shifted.\nIn the next cycles it will remain in the shift state');
 gettext('01_four_bits_fixed_binary_value');
-/// 2. Four bits fixed hexa value
+gettext('Displaying a 4-bits fixed value in the leds');
 gettext('02_four_bits_fixed_hexa_value');
-/// 3. Eight bits fixed value buses
+gettext('Displaying a 4-bits fixed value (in hexadecimal) in the leds');
 gettext('03_eight_bits_fixed_value_buses');
-/// 4. Eight bits counter
+gettext('Using buses for displaying a fixed value in the leds');
 gettext('04_eight_bits_counter');
-/// 5. Split buses
+gettext('An 8-bit binary counter');
 gettext('05_split_buses');
-/// 6. ROM 16x8
+gettext('An 8-bit binary counter, using an 8-bit bus splitted\ninto two 4-bit buses');
 gettext('06_rom16x8');
-/// 7. ROM 16x8 file
+gettext('Displaying the contents of an 16x8 rom memory in the leds');
 gettext('07_rom16x8_file');
-/// 8. ROM 16x8 file generic
+gettext('Displaying the contents of an 16x8 rom memory in the leds\nThe memory is read from the rom0.list file');
 gettext('08_rom16x8_file_generic');
-
-
-
-
-
-
-
-
-
